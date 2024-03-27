67b4655fe6a78e9c8d529d93c5546aaa27d2ca85
Ivan Radanov Ivanov <ivanov2@llnl.gov>
2024-03-27 02:40:18 -0700

    add py script

---
+++ scripts/input-gen-module.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import subprocess
+import argparse
+import os
+import sys
+
+if __file__ == '__main__':
+    parser = argparse.ArgumentParser('InputGen')
+
+    parser.add_argument('--outputdir', required=True)
+    parser.add_argument('--inputmodule', required=True)
+    parser.add_argument('--num', type=int, default=1)
+    parser.add_argument('--rt', default='../rt.cpp')
+
+    args = parser.parse_args()
+
+    os.makedirs(args.targetdir)
+
+    subprocess.run([
+        'input-gen',
+        '--input-gen-runtime', args.rt,
+        '--output-dir', args.outputdir,
+        args.inputmodule,
+        '--compile-input-gen-executable',
+    ])
+
+    with open(os.path.join(args.outputdir, 'available_functions')) as available_functions_file:
+        for func in available_functions_file.readlines():
+            inputgen_exe = os.path.join(args.outputdir, 'input-gen.' + func + '.executable')
+            func_inputs_dir = os.path.join(args.targetdir, 'input-gen.' + func + '.inputs')
+            os.makedirs(func_inputs_dir)
+            subprocess.run([
+                inputgen_exe,
+                func_inputs_dir,
+                '0', str(args.num)
+            ])
