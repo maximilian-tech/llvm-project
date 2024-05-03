720952b0259d683681c9f9bab4ed818a37ce75be
Ivan Radanov Ivanov <ivanov2@llnl.gov>
2024-05-02 22:23:45 -0700

    Fix scripts

--- scripts/run_local_mass_input_gen.sh
+++ scripts/run_local_mass_input_gen.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 set -x
@@ -8,7 +8,14 @@ module load python/3.10.8
 START=0
 END=7000
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CURDATE=$(date +"%Y-%m-%dT%H:%M:%S%z")
+JOB_LOG_DIR="$LCWS/results/jobs/$CURDATE/"
+mkdir -p "$JOB_LOG_DIR"
+JOB_NAME="compile-inputgen"
 JOB_LOG="$JOB_LOG_DIR/job-$JOB_NAME.main.out"
+
+echo tail -f "$JOB_LOG"
 
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -35,4 +42,4 @@ NUM_CPU=40
     --input-gen-num-retries 5 \
     --input-gen-timeout 5 \
     --input-run-timeout 5 \
-    --num-procs="$NUM_CPU" \
+    --num-procs="$NUM_CPU" &> "$JOB_LOG"
