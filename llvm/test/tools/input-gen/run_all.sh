171f58c408753b073b5546d28b231a32f1b2bc18
Ivan Radanov Ivanov <ivanov2@llnl.gov>
2024-05-06 18:17:41 -0700

    Do not litter cwd in tests

--- llvm/test/tools/input-gen/run_all.sh
+++ llvm/test/tools/input-gen/run_all.sh
@@ -3,11 +3,11 @@ set -e
 
 while read -r func_id func_name ; do
   gen="$1/input-gen.module.generate.a.out"
-  "$gen" "$1" 0 2 "$func_name" "$func_id" 2> "$func_id.err.out" > "$func_id.log.out"
+  "$gen" "$1" 0 2 "$func_name" "$func_id" 2> "$1/$func_id.gen.err.out" > "$1/$func_id.gen.log.out"
 
   run="$1/input-gen.module.run.a.out"
   for i in "$gen".input."$func_id".*.bin; do
-    "$run" "$i" "$func_name" 2> "$i".err.out > "$i".log.out
+    "$run" "$i" "$func_name" 2> "$i".run.err.out > "$i".run.log.out
   done;
 
 done < "$1/available_functions"
